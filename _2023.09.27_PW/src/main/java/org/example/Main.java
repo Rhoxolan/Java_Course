dd7c1e6a75c12bef3eb969fe6700eafd040270ad
Rhoxolan <pba17mail@gmail.com>
2023-10-07 20:00:33 +0300

    The first practical work was completed

--- _2023.09.27_PW/src/main/java/org/example/Main.java
+++ _2023.09.27_PW/src/main/java/org/example/Main.java
@@ -2,63 +2,95 @@ package org.example;
 
 import java.util.Scanner;
 
+interface Task {
+    void doTask();
+}
+
 public class Main {
     public static void main(String[] args) {
         practicalWork();
     }
 
     private static void practicalWork() {
-        while (true){
-            task1();
-        }
+        short choice;
+        do {
+            System.out.println("\nPlease select the task (1 - 12)\n0 - Exit");
+            Scanner scanner = new Scanner(System.in);
+            choice = scanner.nextShort();
+            scanner.nextLine();
+            System.out.println("\n\n\n\n\n");
+            doTask(choice);
+        } while (choice != 0);
+    }
+
+    private static void doTask(short index) {
+        Tasks()[index].doTask();
+        pressEnter();
+    }
+
+    private static Task[] Tasks() {
+        Task[] tasks = new Task[12];
+        tasks[0] = Main::task1;
+        tasks[1] = Main::task2;
+        tasks[2] = Main::task3;
+        tasks[3] = Main::task4;
+        tasks[4] = Main::task5;
+        tasks[5] = Main::task6;
+        tasks[6] = Main::task7;
+        tasks[7] = Main::task8;
+        tasks[8] = Main::task9;
+        tasks[9] = Main::task10;
+        tasks[10] = Main::task11;
+        tasks[11] = Main::task12;
+        return tasks;
     }
 
     private static void task1() {
-        pressEnter();
+
     }
 
     private static void task2() {
-        pressEnter();
+
     }
 
     private static void task3() {
-        pressEnter();
+
     }
 
     private static void task4() {
-        pressEnter();
+
     }
 
     private static void task5() {
-        pressEnter();
+
     }
 
     private static void task6() {
-        pressEnter();
+
     }
 
     private static void task7() {
-        pressEnter();
+
     }
 
     private static void task8() {
-        pressEnter();
+
     }
 
     private static void task9() {
-        pressEnter();
+
     }
 
     private static void task10() {
-        pressEnter();
+
     }
 
     private static void task11() {
-        pressEnter();
+
     }
 
     private static void task12() {
-        pressEnter();
+
     }
 
     private static void pressEnter() {
