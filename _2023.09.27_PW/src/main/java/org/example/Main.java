c912562f3257ef841138acadb3f72e6609cca485
Rhoxolan <pba17mail@gmail.com>
2023-10-07 11:34:02 +0300

    The first practical work was supplemented

--- _2023.09.27_PW/src/main/java/org/example/Main.java
+++ _2023.09.27_PW/src/main/java/org/example/Main.java
@@ -1,7 +1,70 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        practicalWork();
+    }
+
+    private static void practicalWork() {
+        while (true){
+            task1();
+        }
+    }
+
+    private static void task1() {
+        pressEnter();
+    }
+
+    private static void task2() {
+        pressEnter();
+    }
+
+    private static void task3() {
+        pressEnter();
+    }
+
+    private static void task4() {
+        pressEnter();
+    }
+
+    private static void task5() {
+        pressEnter();
+    }
+
+    private static void task6() {
+        pressEnter();
+    }
+
+    private static void task7() {
+        pressEnter();
+    }
+
+    private static void task8() {
+        pressEnter();
+    }
+
+    private static void task9() {
+        pressEnter();
+    }
+
+    private static void task10() {
+        pressEnter();
+    }
+
+    private static void task11() {
+        pressEnter();
+    }
+
+    private static void task12() {
+        pressEnter();
+    }
+
+    private static void pressEnter() {
+        System.out.println("\nPress ENTER");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("\n\n\n\n\n");
     }
 }
