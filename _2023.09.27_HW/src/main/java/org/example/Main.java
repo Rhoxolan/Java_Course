328e97dcbffa8d493c6c228d349dcdf0fb5b83c4
Rhoxolan <pba17mail@gmail.com>
2023-10-21 22:19:59 +0300

    The first homework was supplemented

--- _2023.09.27_HW/src/main/java/org/example/Main.java
+++ _2023.09.27_HW/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -113,7 +112,33 @@ class Tasks {
     }
 
     public static void task5() {
-
+        System.out.println("Please enter the month:");
+        Scanner scanner = new Scanner(System.in);
+        int monthNumber = scanner.nextInt();
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Winter!");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring!");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer!");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn!");
+                break;
+            default:
+                System.out.println("The value isn`t valid!");
+        }
     }
 
     public static void task6() {
